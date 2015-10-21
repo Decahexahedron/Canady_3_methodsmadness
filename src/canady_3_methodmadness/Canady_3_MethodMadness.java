69129b5441a2a227c2e5c10c255b86263fcadbb3
Rhys Canady <rhysdaniel064@gmail.com>
2015-10-21 12:15:29 -0500

    final methods

--- src/canady_3_methodmadness/Canady_3_MethodMadness.java
+++ src/canady_3_methodmadness/Canady_3_MethodMadness.java
@@ -11,7 +11,7 @@ import javafx.util.Duration;
 
 //translate and graphStar ended up not used much if at all
 public class Canady_3_MethodMadness extends Application {
-//public static int counter = 0;
+
     public static final double W = 1400; // canvas dimensions.
     public static final double H = 1050;
     public static final double D = 20;  // diameter.
@@ -25,8 +25,7 @@ public class Canady_3_MethodMadness extends Application {
                 new KeyFrame(Duration.seconds(0),
                         new KeyValue(x, 0),
                         new KeyValue(y, 0)
-//                        counter += 1;
-                        
+
                 ),
                 new KeyFrame(Duration.seconds(2),
                         new KeyValue(x, W - D),
@@ -41,15 +40,11 @@ public class Canady_3_MethodMadness extends Application {
             @Override
             public void handle(long now) {
                 GraphicsContext gc = canvas.getGraphicsContext2D();
-
-                
                 int i = (int) x.doubleValue();
                 int a = (int) y.doubleValue();
 
-                    triangle.drawTriangle(gc, i, a);
-                }
-
-            
+                triangle.drawTriangle(gc, i, a);
+            }
         };
 
         stage.setScene(
@@ -60,7 +55,6 @@ public class Canady_3_MethodMadness extends Application {
                 )
         );
         stage.show();
-
         timer.start();
         timeline.play();
     }
